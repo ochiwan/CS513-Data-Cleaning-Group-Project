258085bbbf8feae581f6e6cb222971893166fc00
Syed Hassan <syedhassan@Syeds-MacBook-Pro.local>
2022-07-30 20:25:19 -0500

    run script for Task 3

--- Subtasks/Task_3/CS513_GroupProject_Task3.py
+++ Subtasks/Task_3/CS513_GroupProject_Task3.py
@@ -14,17 +14,15 @@ import os
 
 dir = str(os.getcwd())
 
-# @BEGIN Task3
-# @IN filepath @URI file:~/Documents/Data Cleaning Project CS513/CS513-Data-Cleaning-Group-Project/Subtasks/Task_2/2022-Jul-16/Task2.1_quarterly_and_yearly_data_postPandas.csv
-# @IN filepath @URI file:~/Documents/Data Cleaning Project CS513/CS513-Data-Cleaning-Group-Project/Subtasks/Task_2/2022-Jul-22/Task2.2_monthly_data_postPandas.csv#
-# @OUT Task3_monthly_data.csv @URI file:Task3_monthly_data.csv
-# @OUT Task3_quarterly_and_yearly_data.csv @URI file:Task3_quarterly_and_yearly_data.csv
-
 # Read in the csv
-#username = "shassan"                      # your username here (windows)
+#username = "shassan"
 #wFolder = "Data Cleaning Project CS513" # working folder for the project here
-filepath_Jul16 = os.path.expanduser('~/Documents/Data Cleaning Project CS513/CS513-Data-Cleaning-Group-Project/Subtasks/Task_2/2022-Jul-16/Task2.1_quarterly_and_yearly_data_postPandas.csv')
-filepath_Jul22 = os.path.expanduser('~/Documents/Data Cleaning Project CS513/CS513-Data-Cleaning-Group-Project/Subtasks/Task_2/2022-Jul-22/Task2.2_monthly_data_postPandas.csv')
+
+# @BEGIN Task3
+# @IN filepath @URI file:~/Desktop/projects/CS513-Data-Cleaning-Group-Project/Subtasks/Task_2/2022-Jul-22/Task2.2_monthly_data_postPandas.csv#
+# @OUT Task3_quarterly_and_yearly_data.csv @URI file:Task3_quarterly_and_yearly_data.csv
+filepath_Jul16 = os.path.expanduser('~/Desktop/projects/CS513-Data-Cleaning-Group-Project/Subtasks/Task_2/2022-Jul-16/Task2.1_quarterly_and_yearly_data_postPandas.csv')
+filepath_Jul22 = os.path.expanduser('~/Desktop/projects/CS513-Data-Cleaning-Group-Project/Subtasks/Task_2/2022-Jul-22/Task2.2_monthly_data_postPandas.csv')
 
 df_Jul16 = pd.read_csv(filepath_Jul16)
 df_Jul22 = pd.read_csv(filepath_Jul22)
@@ -37,34 +35,31 @@ stock_market_Jul22 = df_Jul22
 
 # Get stock market total
 # @BEGIN getStockMarketTotal
-# @PARAM df
-# @RETURN df
-def getStockMarketTotal(df):
+# @PARAM stock_market_df
+# @OUT stockMarketTotal
+def getStockMarketTotal(stock_market_df):
     US_stockMarket_thisYear = 0
     field =  'Stock Markets, US$, Billions'
 
     index = 0
-    for i in range(0, len(df)-10):   
+    for i in range(0, len(stock_market_df)-10):   
         if i > 0 and ((i + 1) % 10) == 0:
             index = i+10
-        US_stockMarket_thisYear = df.loc[index, field]
+        US_stockMarket_thisYear = stock_market_df.loc[index, field]
 
-        if isinstance(US_stockMarket_thisYear, str) or isinstance(df.loc[i,field],str):
-            df.loc[i,'US Stock Market to Stock Market'] = ''
+        if isinstance(US_stockMarket_thisYear, str) or isinstance(stock_market_df.loc[i,field],str):
+            stock_market_df.loc[i,'US Stock Market to Stock Market'] = ''
         else:
-            df.loc[i,'US Stock Market to Stock Market'] = df.loc[i,field] / US_stockMarket_thisYear
+            stock_market_df.loc[i,'US Stock Market to Stock Market'] = stock_market_df.loc[i,field] / US_stockMarket_thisYear
         
-    df = df.fillna('')
-    return df
+    stock_market_df = stock_market_df.fillna('')
+    return stock_market_df
 # @END getStockMarketTotal
-
-stock_market_Jul16 = getStockMarketTotal(stock_market_Jul16)
-stock_market_Jul22 = getStockMarketTotal(stock_market_Jul22)
 
 # Read in the csv
 # @BEGIN getImportExportRelations
-# @PARAM df
-# @OUT df
+# @PARAM stock_market_df
+# @OUT importExportRelations
 def getImportExportRelations(df):
     US_importExport_thisYear = 0
     US_exportImport_thisYear = 0
@@ -118,13 +113,10 @@ def getImportExportRelations(df):
     return df
 # @END getImportExportRelations
 
-stock_market_Jul16 = getImportExportRelations(stock_market_Jul16)
-stock_market_Jul22 = getImportExportRelations(stock_market_Jul22)
-
 # Correlation of GDP to unemployment rate
 # @BEGIN getUSAUnemploymentRate @DESC Get unemployment rate for USA
-# @IN df
-# @OUT df
+# @PARAM stock_market_Jul16
+# @OUT USAUnemploymentRateJul16
 def getUSAUnemploymentRate(df):
     US_unemployment_thisYear = 0
     field =  'Unemployment rate,Percent,,,'
@@ -144,20 +136,62 @@ def getUSAUnemploymentRate(df):
     return df
 # @END getUSAUnemploymentRate
 
+# Stock Market Total
+# @BEGIN stock_market_total_jul16 @DESC Compute stock market total for July 16
+# @IN df_jul16
+# @OUT df_jul16_2
+stock_market_Jul16 = getStockMarketTotal(stock_market_Jul16)
+# @END stock_market_total_jul16
+
+# Stock Market Total
+# @BEGIN stock_market_total_jul22 @DESC Compute stock market total for July 22
+# @IN df_jul22
+# @OUT df_jul22_2
+stock_market_Jul22 = getStockMarketTotal(stock_market_Jul22)
+# @END stock_market_total_jul22
+
+# Import Export Relations
+# @BEGIN import_export_relations_jul16 @DESC Compute import export relations for July 16
+# @IN df_jul16_2
+# @OUT df_jul16_3
+# stock_market_Jul16 = getImportExportRelations(stock_market_Jul16)
+# @END import_export_relations_jul16
+
+# Import Export Relations
+# @BEGIN import_export_relation_jul22 @DESC Compute import export relations for July 22
+# @IN df_jul22_2
+# @OUT df_jul22_3
+# stock_market_Jul22 = getImportExportRelations(stock_market_Jul22)
+# @END import_export_relation_jul22
+
+# Import Export Relations
+# @BEGIN USAUnemploymentRate_jul16 @DESC Compute import export relations for July 22
+# @IN df_july16_3
+# @OUT df_jul16_4
 stock_market_Jul16 = getUSAUnemploymentRate(stock_market_Jul16)
+# @END USAUnemploymentRate_jul16
+
+# Import Export Relations
+# @BEGIN USAUnemploymentRate_jul22 @DESC Compute import export relations for July 22
+# @IN df_jul22_3
+# @OUT df_jul22_4
 stock_market_Jul22 = getUSAUnemploymentRate(stock_market_Jul22)
+# @END USAUnemploymentRate_jul22
 
-print(stock_market_Jul22)
+savePath = os.path.expanduser('~/Desktop/projects/CS513-Data-Cleaning-Group-Project/Subtasks/Task_3')
 
-# Export the files as csv
+# Export the file as csv
 # @BEGIN export_as_csv
-# @IN df
-# @OUT Task3_monthly_data.csv
+# @IN df1
 # @OUT Task3_quarterly_and_yearly_data.csv
-savePath = os.path.expanduser('~/Documents/Data Cleaning Project CS513/CS513-Data-Cleaning-Group-Project/Subtasks/Task_3')
 stock_market_Jul16.to_csv(savePath+"/Task3_quarterly_and_yearly_data.csv")
+# @END export_as_csv
+
+# Export the file as csv
+# @BEGIN export_as_csv
+# @IN df2
+# @OUT Task3_monthly_data.csv
 stock_market_Jul22.to_csv(savePath+"/Task3_monthly_data.csv")
 # @END export_as_csv
 
 # @END Task3
-
